1d87d78325359dcf9d694c6f03105005b108627c
reelooz <jnpitet@gmail.com>
2013-11-20 16:34:45 +0100

    suppr maxime

--- src/méthodeagiletp1/MéthodeAgileTP1.java
+++ src/méthodeagiletp1/MéthodeAgileTP1.java
@@ -16,7 +16,7 @@ public class MéthodeAgileTP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println ("Hello Maxime thomas Parakeet Jean-Hugues deathpote");
+        System.out.println ("Hello thomas Parakeet Jean-Hugues deathpote");
     }
     
     
